67d86dafb665d00b37d56fc863979e3c27b548c9
Andrey <Andrey@DESKTOP-SNT4IHB>
2017-03-28 21:57:10 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct Link
+{
+    int weight;
+    Node *
+};
+
+struct Links
+{
+    Link *prev;
+    Link *next;
+    Links *next;
+};
+
+struct Node
+{
+    char name;
+    int dist;
+    Node *next;
+    Links *
+};
+
+int main(void)
+{
+    FILE *fp;
+
+
+    fp = getFilePtr();
+
+    
+    return 0;
+}
